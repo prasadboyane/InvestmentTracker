031ea085ed0a9b8a241bd197bcb8bfac9d4784b6
Prasad Boyane <Prasad.Boyane@datametica.com>
2020-05-17 00:20:29 +0530

    added all basic functions

--- Tracker_main.py
+++ Tracker_main.py
@@ -2,6 +2,7 @@ import openpyxl
 import os.path
 import time
 from datetime import datetime
+import matplotlib.pyplot as plt
 
 #filepath = "/home/ubun/Desktop/stocksinfo/test101.xlsx"
 
@@ -79,15 +80,62 @@ def insert_entry():
         exit()
 
 def update_entry():
-    id_entry = input('Please enter entry id: ')
-    
+    id_entry = input('Please enter id to update: ')
+    id_entry_int=int(id_entry)+1
+    id_entry_1=str(id_entry_int)
     
     wb = openpyxl.load_workbook('Tracker_enc.xlsx')
     data_sheet = wb["data"]
+    i_a = 'A{}'.format(id_entry_1)
+    i_b = 'B{}'.format(id_entry_1)
+    i_c = 'C{}'.format(id_entry_1)
+    i_d = 'D{}'.format(id_entry_1)
+    i_e = 'E{}'.format(id_entry_1)
     
-    row = (amount_entry,date_entry)
-    data_sheet.append(row)
+    msg="Here's your record: ",data_sheet[i_a].value,data_sheet[i_b].value,data_sheet[i_c].value,data_sheet[i_d].value,data_sheet[i_e].value
+    print(msg)
+    print('which field you want to edit?')
+    print('1. amount')
+    print('2. date')
+    print('3. assetname')
+    print('4. maturity date')
+    print('5. Back')
+    choice=input('Enter choice number: ')
+    
+    if choice=='1':
+        updated_val=input('Enter updated value for amount: ')
+        data_sheet[i_b] = updated_val
+    elif choice =='2':
+        updated_val=input('Enter updated value for date(YYYY-MM-DD): ')
+        try:
+            year, month, day = map(int, updated_val.split('-'))
+            updated_val_1 = datetime(year, month, day)
+        except:
+            print('Please valid enter date in correct format !')
+            update_entry()
+        data_sheet[i_c] = updated_val_1
+    elif choice =='3':
+        updated_val=input('Enter updated value for assetname: ')
+        data_sheet[i_d] = updated_val
+    elif choice =='4':
+        updated_val=input('Enter updated value for Maturity date(YYYY-MM-DD): ')
+        try:
+            year, month, day = map(int, updated_val.split('-'))
+            updated_val_1 = datetime(year, month, day)
+        except:
+            print('Please valid enter date in correct format !')
+            update_entry()
+        data_sheet[i_c] = updated_val_1
+    elif choice =='5':
+        show_menu()
+        
     wb.save('Tracker_enc.xlsx') 
+    
+    go_next=input('Press "m" for Main Menu & "e" for exit ')
+    if go_next=='m':
+        show_menu()
+    else:
+        exit()
 
 
 def view_entry():
@@ -99,6 +147,7 @@ def view_entry():
         for cell in row:
             print(cell.value,'\t\t', end=" ")
         print()
+        
     go_next=input('Press "m" for Main Menu & "e" for exit ')
     if go_next=='m':
         show_menu()
@@ -106,19 +155,80 @@ def view_entry():
         exit()
 
 
+def delete_entry():
+    wb = openpyxl.load_workbook('Tracker_enc.xlsx')
+    ws = wb["data"]
+    
+    id_entry = input('Please enter id to delete: ')
+    ws.delete_rows(int(id_entry)+1)
+    wb.save('Tracker_enc.xlsx')
+    
+    go_next=input('Press "m" for Main Menu & "e" for exit ')
+    if go_next=='m':
+        show_menu()
+    else:
+        exit()
+    
+
+def view_graph():
+    wb = openpyxl.load_workbook('Tracker_enc.xlsx')
+    ws = wb["data"]
+    
+    date_arr=[]
+    colC = ws['C']
+    for cell in colC:
+        if cell.value != 'date':
+            date_arr.append(cell.value)
+    print()    
+    
+    date_arr.sort()
+    min_date=min(date_arr)
+    max_date=max(date_arr)
+    
+    amount_arr=[]
+    colB = ws['B']
+    for cell in colB:
+        if cell.value != 'amount':
+            amount_arr.append(cell.value)
+    print()    
+    
+    amount_arr.sort()
+    min_am=min(amount_arr)
+    max_am=max(amount_arr)
+    
+    
+    plt.plot(date_arr, amount_arr,'go--', linewidth=1, markersize=2)
+    plt.axis([min_date, max_date, min_am, max_am])
+    plt.ylabel('Amount')
+    plt.show()
+    
+    go_next=input('Press "m" for Main Menu & "e" for exit ')
+    if go_next=='m':
+        show_menu()
+    else:
+        exit()
+    
+
 def show_menu():
     print('******   1. Insert new Entry ******')
     print('******   2. Update Entry     ******')
     print('******   3. Delete Entry     ******')
-    print('******   4. View Graph       ******')
-    print('******   5. Exit             ******')
+    print('******   4. View Entries     ******')
+    print('******   5. View Graph       ******')
+    print('******   6. Exit             ******')
     choice = input('Please enter your choice: ')
     if choice == '1':
         print('choice 1')
         insert_entry()
+    elif choice == '2':
+        update_entry()
+    elif choice == '3':
+        delete_entry()
     elif choice == '4':
         view_entry()
     elif choice == '5':
+        view_graph()
+    elif choice == '6':
         exit()
 
 ##START PROGRAM HERE
